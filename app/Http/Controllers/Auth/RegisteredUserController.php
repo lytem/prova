9006edb96583487f0760252a403436ca367928d1
lytem <lytem85@gmail.com>
2021-12-01 12:15:16 +0100

    default role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,6 +44,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $user->assignRole('writer');
 
         event(new Registered($user));
 
