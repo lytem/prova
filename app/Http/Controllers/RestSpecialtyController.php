587207529503fa0448f3d0af77a9d50713b3cd1e
lytem <lytem85@gmail.com>
2021-12-04 22:13:21 +0100

    api specialty

---
+++ app/Http/Controllers/RestSpecialtyController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Specialty;
+use Illuminate\Http\Request;
+
+class RestSpecialtyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(
+            Specialty::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $specialty=new Specialty();
+        $name= $request->input('nome');
+
+        $specialty->nome=$name;
+
+        $specialty->save();
+        return response("",201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Specialty  $specialty
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Specialty $specialty)
+    {
+        return response()->json($specialty);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Specialty  $specialty
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Specialty $specialty)
+    {
+        $specialty->nome=$request->input('nome');
+        $specialty->update();
+
+        return response("",204);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Specialty  $specialty
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Specialty $specialty)
+    {
+        $specialty->delete();
+    }
+}
